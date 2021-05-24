7a9a07f91fd87e9acc2bcd818c7f6b65cf4d7e60
Nathan Bienvenu <45998296+NathanBnvn@users.noreply.github.com>
2021-05-24 18:53:43 +0200

    Structuration du code
    Changement d'approche pour un résultat plus nette.
    - ajout des fenêtres modales fonctionnelles
    - modification d'un filtre pour la gestion des films

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-window.onload = fetchURL(), manageCarrousel(); 
+window.onload = fetchURL(), manageCarrousel(), closeModal(); 
 
 let urls = []
-let pictures = []
+var pictures = []
+var movies = []
+let maxResults = 40
 let surplus = [8, 9, 17, 18, 19, 27, 28, 29, 37, 38, 39, 40]
 
 // Fait bloucler les diverses urls dans fetch
@@ -19,7 +21,7 @@ async function fetchURL(){
 	}
 }
 
-// Récupère les données que je stocke dans un tableau pour mieux appeler les urls
+// Récupère les données que je stocke dans un tableau pour mieux appeler les urls puis je crée un tableau par film
 
 async function fetchMovies(results){
 	for(let n = 0; n < results.length; n++){
@@ -27,66 +29,86 @@ async function fetchMovies(results){
 		urls.push(url)
 	}
 
-	if(urls.length == 40){
+	if(urls.length == maxResults){
 		for (var i = 0; i < urls.length; i++) {
 		 	var movie = await fetch(urls[i])
 		 	var data = await movie.json()
-		 	parseImages(data)
+		 	this["movie" + i] = [data.image_url, data.title, data.genres, data.date_published, data.rated, data.imdb_score, data.directors, data.actors, data.duration, data.countries, data.worldwide_gross_income, data.long_description]
+		}
+		loadTopMovie()
+		parseImages()
+	}
+}
+
+// Je filtre l'ensemble de mes résultats pour garder les 29 attendus
+
+function parseImages(){
+	var filter = surplus.find(excedent => excedent == i)
+	photoframe = document.querySelectorAll('img')
+
+	for(var i = 0; i < maxResults; i++){
+		var filter = surplus.find(excedent => excedent == i)
+		if(! (i == filter)){
+			picture = this["movie" + i][0]
+			movie = this["movie" + i]
+			pictures.push(picture)
+			movies.push(movie)
 		}
 	}
 
-}
+	for(var n = 0; n < 29; n++){
+		photoframe = document.querySelectorAll('img')[n]
+		photoframe.src = pictures[n]
 
-// Rassemble les urls des images qui sont ensuite filtrer puis afficher
-
-function parseImages(data){
-	pictures.push(data.image_url)
-	
-	if(pictures.length == 40){
-		var images = pictures.filter(function(element, index){
-			return surplus.indexOf(index) == -1
+		if(n > 0){
+			photoframe.addEventListener("click", function(){
+			openModal()
 		})
-
-		for(let i = 0; i < images.length; i++) {
-			picture = document.querySelectorAll('img')[i];
-			picture.src = images[i]
-
-			picture.addEventListener("click", function(){
-				openModal()
-			})
 		}
 	}
 }
 
-function loadTopMovie(titles, images, descriptions){
+function loadTopMovie(){
 	let play = document.getElementById("play-button")
 	
 	title = document.getElementById("title")
-	var titleNode = document.createTextNode("")
-	title.appendChild(titleNode)
+	title.textContent = movie0[1]
 
 	description = document.getElementById("description")
-	var descriptionNode = document.createTextNode("")
-	description.appendChild(descriptionNode)
+	description.textContent = movie0[11]
 
 	play.onclick = function(){
 		openModal()
 	}
 }
 
-// gère l'ouverture des fenêtres modals
+// Gère l'ouverture des fenêtres modals et l'affichage de celles-ci
 
 function openModal(){
 	modal.style.display = "block"
-	
+	var eventSender = event.currentTarget
 
-	// console.log(pictures)
+	var imageModal = document.getElementById("modal-picture")
+	var titleModal = document.getElementById("modal-title")
+	var paragraph = document.getElementById("modal-paragraph")
+	paragraph.setAttribute("style", "white-space: pre")
 
-	// pictures.forEach(imageButton => {
-	//  	imageButton.addEventListener("click", () => {
-	//  		modal.style.display = "block"
-	//  	})
-	// })
+	if(eventSender.id == "play-button"){
+		imageModal.src = movies[0][0]
+		titleModal.textContent = movies[0][1]
+		paragraph.textContent = "Genre: "+ movies[0][2] +", Date de sortie: "+ movies[0][3] +", Durée: " + movies[0][8] +"\r\n"
+		paragraph.textContent += "Note: "+ movies[0][4] +", Score IMDB: "+ movies[0][5] + ", Box Offices: "+ movies[0][10] +"\r\n"
+		paragraph.textContent += "Producteur: " + movies[0][6] + ", Acteurs: "+ movies[0][7] + ", Origine: "+ movies[0][9] +"\r\n"
+		paragraph.textContent += "Synopsis: \r\n" + movies[0][11]
+
+	} else {
+		imageModal.src = movies[eventSender.id][0]
+		titleModal.textContent =  movies[eventSender.id][1]
+		paragraph.textContent = "Genre: "+ movies[eventSender.id][2] +", Date de sortie: "+ movies[eventSender.id][3] +", Durée: " + movies[eventSender.id][8] +"\r\n"
+		paragraph.textContent += "Note: "+ movies[eventSender.id][4] +", Score IMDB: "+ movies[eventSender.id][5] + ", Box Offices: "+ movies[eventSender.id][10] +"\r\n"
+		paragraph.textContent += "Producteur: " + movies[eventSender.id][6] + ", Acteurs: "+ movies[eventSender.id][7] + ", Origine: "+ movies[eventSender.id][9] +"\r\n"
+		paragraph.textContent += "Synopsis: \r\n" + movies[eventSender.id][11]
+	}
 }
 
 function closeModal(){
